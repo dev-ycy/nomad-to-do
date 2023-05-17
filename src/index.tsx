23d6c6deefd911bd144b99fadb9e1791e415bbbc
eblovemw <eblovemw@naver.com>
2023-05-17 17:28:25 +0900

    #3 router 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import router from "./router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <RecoilRoot>
+      <RouterProvider router={router} />
       <App />
     </RecoilRoot>
   </React.StrictMode>
